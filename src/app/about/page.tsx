594961fff79168594110ccc508cdfad251110e58
Florian Mealing <florianmealing@gmail.com>
2025-05-06 11:13:49 +0100

    Implemented Alis picture

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,7 +9,7 @@ const teamMembers = [
   { name: "Florian Mealing", image: "/images/team/florian.jpg" },
   { name: "Nouraldeen Abdelmajeed", image: "/images/team/nouraldeen.jpg" },
   { name: "Alfie Hands", image: "/images/team/alfie.jpeg" },
-  { name: "Ali Dharsee", image: "/images/team/ali.jpg" },
+  { name: "Ali Dharsee", image: "/images/team/ali1.jpg" },
   { name: "Ihsaan Ahmed", image: "/images/team/ihsaan.jpeg" },
   { name: "Arsalan Khan", image: "/images/team/arsalan.jpg" },
   { name: "Chris Jones", image: "/images/team/chrisj.jpg" },
